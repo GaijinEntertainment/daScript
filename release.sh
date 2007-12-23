1d228d29ec907519a8b3db975a8e742d73cefc15
Sebastian Pipping <sebastian@pipping.org>
2007-12-23 18:07:44 +0000

    Todo list and and release script updates
    git-svn-id: https://uriparser.svn.sourceforge.net/svnroot/uriparser/uriparser/trunk@267 119732b9-2324-0410-a8a5-815c10060bbe

--- release.sh
+++ release.sh
@@ -1,18 +1,8 @@
-#!/bin/bash
+#! /bin/sh
 
 INSTALL_DIR="${HOME}/install"
 PREFIX="--prefix=${INSTALL_DIR}"
-## PREFIX=""
-
-MAKE_PARAMS="-j10"
-## MAKE_PARAMS=""
-
-## MY_ACLOCAL="/usr/bin/aclocal"
-MY_ACLOCAL="aclocal"
-## MY_AUTOCONF="/usr/bin/autoconf"
-MY_AUTOCONF="autoconf"
-## MY_AUTOMAKE="/usr/bin/automake"
-MY_AUTOMAKE="automake"
+MAKE_PARAMS=-j10
 
 
 
@@ -23,38 +13,54 @@ make distclean &> /dev/null
 
 echo
 echo ========== prepare ==========
-${MY_ACLOCAL} \
-&& ${MY_AUTOMAKE} \
-&& ${MY_AUTOCONF} \
-&& echo \
-&& echo ========== configure ========== \
-&& ./configure ${PREFIX} \
-&& echo \
-&& echo ========== make uninstall ========== \
-&& make uninstall \
-&& echo \
-&& echo ========== make ========== \
-&& make ${MAKE_PARAMS} \
-\
-\
-\
-&& echo \
-&& echo ========== make check ========== \
-&& make check \
-\
-\
-\
-&& echo \
-&& echo ========== make install ========== \
-&& make install \
-\
-\
-\
-&& echo \
-&& echo ========== make dist ========== \
-&& make dist \
-\
-\
-\
-&& echo \
-&& echo Fine.
+aclocal || exit 1
+automake || exit 1
+autoconf || exit 1
+
+echo
+echo ========== configure ==========
+./configure ${PREFIX} || exit 1
+
+echo
+echo ========== make uninstall ==========
+make uninstall || exit 1
+
+echo
+echo ========== make ==========
+make ${MAKE_PARAMS} || exit 1
+
+
+
+echo
+echo ========== make check ==========
+make check || exit 1
+
+
+
+echo
+echo ========== make install ==========
+make install || exit 1
+
+
+
+echo
+echo ========== make distcheck ==========
+make distcheck || exit 1
+
+
+
+echo
+echo Fine.
+
+echo
+echo <<'CHECKLIST'
+* run ./edit_version.sh
+* updated the soname
+* updated file lists
+  - Makefile.am
+  - Code::Blocks
+  - Visual Studio 2005
+* searched for TODO inside code using
+  grep -R "TODO" include/* src/* test/*
+?
+CHECKLIST
