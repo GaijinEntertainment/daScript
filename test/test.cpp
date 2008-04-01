90fdef0e6d9009c6179346c588489d18ac221189
Sebastian Pipping <sebastian@pipping.org>
2008-04-01 21:49:06 +0000

    Another test case
    git-svn-id: https://uriparser.svn.sourceforge.net/svnroot/uriparser/uriparser/trunk@329 119732b9-2324-0410-a8a5-815c10060bbe

--- test/test.cpp
+++ test/test.cpp
@@ -1327,6 +1327,7 @@ private:
 	void testQueryListPair() {
 		testQueryListPairHelper("one+two+%26+three=%2B", "one two & three", "+");
 		testQueryListPairHelper("one=two=three", "one", "two=three", "one=two%3Dthree");
+		testQueryListPairHelper("one=two=three=four", "one", "two=three=four", "one=two%3Dthree%3Dfour");
 	}
 
 };
