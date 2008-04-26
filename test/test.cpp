ab6e830a3e3cf8bc9596e4a0acdb9c9ff306223e
Sebastian Pipping <sebastian@pipping.org>
2008-04-26 17:28:09 +0000

    Another test for bug #1950126
    git-svn-id: https://uriparser.svn.sourceforge.net/svnroot/uriparser/uriparser/trunk@343 119732b9-2324-0410-a8a5-815c10060bbe

--- test/test.cpp
+++ test/test.cpp
@@ -950,8 +950,21 @@ private:
 	}
 
 	void testToString_Bug1950126() {
-		TEST_ASSERT(testToStringHelper(L"http://e.com/"));
-		TEST_ASSERT(testToStringHelper(L"http://e.com"));
+		UriParserStateW state;
+		UriUriW uriOne;
+		UriUriW uriTwo;
+		const wchar_t * const uriOneString = L"http://e.com/";
+		const wchar_t * const uriTwoString = L"http://e.com";
+		state.uri = &uriOne;
+		TEST_ASSERT(URI_SUCCESS == uriParseUriW(&state, uriOneString));
+		state.uri = &uriTwo;
+		TEST_ASSERT(URI_SUCCESS == uriParseUriW(&state, uriTwoString));
+		TEST_ASSERT(URI_FALSE == uriEqualsUriW(&uriOne, &uriTwo));
+		uriFreeUriMembersW(&uriOne);
+		uriFreeUriMembersW(&uriTwo);
+
+		TEST_ASSERT(testToStringHelper(uriOneString));
+		TEST_ASSERT(testToStringHelper(uriTwoString));
 	}
 
 	bool testToStringCharsRequiredHelper(wchar_t * text) {
