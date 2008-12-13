557e6fb6b927fbf52f45d43f4e6c89299d587dda
Sebastian Pipping <sebastian@pipping.org>
2008-12-13 17:21:19 +0000

    Fix memleak in uriFreeQueryList (first half of #2413066)
    git-svn-id: https://uriparser.svn.sourceforge.net/svnroot/uriparser/uriparser/trunk@384 119732b9-2324-0410-a8a5-815c10060bbe

--- lib/UriQuery.c
+++ lib/UriQuery.c
@@ -336,6 +336,8 @@ UriBool URI_FUNC(AppendQueryItem)(URI_TYPE(QueryList) ** prevNext,
 void URI_FUNC(FreeQueryList)(URI_TYPE(QueryList) * queryList) {
 	while (queryList != NULL) {
 		URI_TYPE(QueryList) * nextBackup = queryList->next;
+		free(queryList->key);
+		free(queryList->value);
 		free(queryList);
 		queryList = nextBackup;
 	}
