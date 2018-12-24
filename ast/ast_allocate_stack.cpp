1135cf5ab3865670125e35a536d4e486bc9f95c6
Boris Batkin <bbatkin@gmail.com>
2018-12-24 11:41:13 -0800

    and even less noise

--- ast/ast_allocate_stack.cpp
+++ ast/ast_allocate_stack.cpp
@@ -13,16 +13,12 @@ namespace yzg {
     protected:
         ProgramPtr              program;
         FunctionPtr             func;
-        vector<VariablePtr>     local;
         uint32_t                stackTop = 0;
         vector<uint32_t>        stackTopStack;
-        vector<size_t>          varStack;
         int                     totalFunctions = 0;
     protected:
         void pushStack()    { stackTopStack.push_back(stackTop); }
         void popStack()     { stackTop = stackTopStack.back(); stackTopStack.pop_back(); }
-        void pushVarStack() { varStack.push_back(local.size()); }
-        void popVarStack()  { local.resize(varStack.back()); varStack.pop_back(); }
     protected:
     // function
         virtual void preVisit ( Function * f ) override {
@@ -37,23 +33,21 @@ namespace yzg {
         }
     // ExprBlock
         virtual void preVisit ( ExprBlock * block ) override {
+            pushStack();
             Visitor::preVisit(block);
-            pushVarStack();
         }
         virtual ExpressionPtr visit ( ExprBlock * block ) override {
-            popVarStack();
+            popStack();
             return Visitor::visit(block);
         }
     // ExprFor
         virtual void preVisit ( ExprFor * expr ) override {
             Visitor::preVisit(expr);
-            pushVarStack();
             pushStack();
         }
         virtual void preVisitForStack ( ExprFor * expr ) override {
             Visitor::preVisitForStack(expr);
             for ( auto & var : expr->iteratorVariables ) {
-                local.push_back(var);
                 var->stackTop = stackTop;
                 stackTop += (var->type->getSizeOf() + 0xf) & ~0xf;
             }
@@ -61,13 +55,11 @@ namespace yzg {
         virtual ExpressionPtr visit ( ExprFor * expr ) override {
             func->totalStackSize = max(func->totalStackSize, stackTop);
             popStack();
-            popVarStack();
             return Visitor::visit(expr);
         }
     // ExprLet
         virtual void preVisit ( ExprLet * expr ) override {
             Visitor::preVisit(expr);
-            pushVarStack();
             pushStack();
         }
         virtual VariablePtr visitLet ( ExprLet * expr, const VariablePtr & var, bool last ) override {
@@ -79,7 +71,6 @@ namespace yzg {
             func->totalStackSize = max(func->totalStackSize, stackTop);
             if ( expr->scoped ) {
                 popStack();
-                popVarStack();
             }
             return Visitor::visit(expr);
         }
