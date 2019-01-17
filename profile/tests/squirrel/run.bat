3d20287e5437a2c96319a04f93c143ee9b2dee73
Aliot <anton@gaijin.ru>
2019-01-17 03:16:05 +0300

    new dict test for squirrel

--- profile/tests/squirrel/run.bat
+++ profile/tests/squirrel/run.bat
@@ -5,9 +5,11 @@ sq3-modified-64.exe primes.nut
 sq3-modified-64.exe particles.nut
 sq3-modified-64.exe native.nut
 sq3-modified-64.exe profile_try_catch.nut 
+sq3-modified-64.exe dict.nut
 
 echo "Squirrel3 original"
 sq3-64.exe fib.nut
 sq3-64.exe primes.nut
 sq3-64.exe particles.nut
+sq3-64.exe dict.nut
 rem sq3-64.exe native.nut
