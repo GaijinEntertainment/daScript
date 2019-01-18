6a05de4574f4f05e450373d2a36b8db00eb90138
Aliot <anton@gaijin.ru>
2019-01-18 20:11:23 +0300

    add luajit2.1.0.beta3. slightly faster

--- profile/tests/lua/run.bat
+++ profile/tests/lua/run.bat
@@ -1,18 +1,18 @@
 echo "LuaJit, no jit"
-luajit.exe -joff fibI.lua
-luajit.exe -joff primes.lua
-luajit.exe -joff particles.lua
-luajit.exe -joff native.lua
-luajit.exe -joff dict.lua 
-luajit.exe -joff profile_try_catch.lua 
+luajit2_1_0beta3\luajit.exe -joff fibI.lua
+luajit2_1_0beta3\luajit.exe -joff primes.lua
+luajit2_1_0beta3\luajit.exe -joff particles.lua
+luajit2_1_0beta3\luajit.exe -joff native.lua
+luajit2_1_0beta3\luajit.exe -joff dict.lua 
+luajit2_1_0beta3\luajit.exe -joff profile_try_catch.lua 
 
 echo "LuaJit, jit"
-luajit.exe fibI.lua
-luajit.exe primes.lua
-luajit.exe particles.lua
-luajit.exe native.lua
-luajit.exe dict.lua 
-luajit.exe profile_try_catch.lua 
+luajit2_1_0beta3\luajit.exe fibI.lua
+luajit2_1_0beta3\luajit.exe primes.lua
+luajit2_1_0beta3\luajit.exe particles.lua
+luajit2_1_0beta3\luajit.exe native.lua
+luajit2_1_0beta3\luajit.exe dict.lua 
+luajit2_1_0beta3\luajit.exe profile_try_catch.lua 
 
 echo "Lua (low res timer, no addOne)"
 lua.exe fibI.lua
