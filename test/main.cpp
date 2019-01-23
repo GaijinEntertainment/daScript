7b93bc1ab72335a244c6def5fa944035cd991817
Boris Batkin <bbatkin@gmail.com>
2019-01-23 10:21:26 -0800

    less noise

--- test/main.cpp
+++ test/main.cpp
@@ -214,7 +214,7 @@ int main() {
     return 0;
 #endif
 #if 0 // Debug this one test
-    unit_test(TEST_PATH "test/hello_world.das");
+    unit_test(TEST_PATH "test/unit_tests/infer_remove_ref_const.das");
     Module::Shutdown();
     return 0;
 #endif
