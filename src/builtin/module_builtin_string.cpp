028f8470fa3494cfeb87fa6d2e36bdf474472d40
Aliot <anton@gaijin.ru>
2019-01-26 01:37:50 +0300

    add string slice

--- src/builtin/module_builtin_string.cpp
+++ src/builtin/module_builtin_string.cpp
@@ -64,11 +64,36 @@ namespace das
         return end != str ? context->heap.allocateString(str, uint32_t(end-str)) : nullptr;
     }
 
+    static inline int clamp_int(int v, int minv, int maxv)
+    {
+        return (v < minv) ? minv : (v > maxv) ? maxv : v;
+    }
+
+    char* builtin_string_slice1 ( const char *str, int start, int end, Context * context )
+    {
+        const uint32_t strLen = stringLengthSafe ( *context, str );
+        if (!strLen)
+            return nullptr;
+        start = clamp_int((start < 0) ? (strLen + start) : start, 0, strLen);
+        end = clamp_int((end < 0) ? (strLen + end) : end, 0, strLen);
+        return end > start ? context->heap.allocateString(str + start, uint32_t(end-start)) : nullptr;
+    }
+    char* builtin_string_slice2 ( const char *str, int start, Context * context )
+    {
+        const uint32_t strLen = stringLengthSafe ( *context, str );
+        if (!strLen)
+            return nullptr;
+        start = clamp_int((start < 0) ? (strLen + start) : start, 0, strLen);
+        return strLen > uint32_t(start) ? context->heap.allocateString(str + start, uint32_t(strLen-start)) : nullptr;
+    }
+
     void Module_BuiltIn::addString(ModuleLibrary & lib) {
         addExtern<DAS_BIND_FUN(builtin_string_endswith)>(*this, lib, "endswith", false);
         addExtern<DAS_BIND_FUN(builtin_string_startswith)>(*this, lib, "startswith", false);
         addExtern<DAS_BIND_FUN(builtin_string_strip)>(*this, lib, "strip", false);
         addExtern<DAS_BIND_FUN(builtin_string_strip_right)>(*this, lib, "strip_right", false);
         addExtern<DAS_BIND_FUN(builtin_string_strip_left)>(*this, lib, "strip_left", false);
+        addExtern<DAS_BIND_FUN(builtin_string_slice1)>(*this, lib, "slice", false);
+        addExtern<DAS_BIND_FUN(builtin_string_slice2)>(*this, lib, "slice", false);
     }
 }
