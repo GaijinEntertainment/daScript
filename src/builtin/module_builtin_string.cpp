74bff8532fb78d9af56215ae972737b591a7a7c9
Aliot <anton@gaijin.ru>
2019-01-28 04:18:52 +0300

    add string to int and string to float conversions

--- src/builtin/module_builtin_string.cpp
+++ src/builtin/module_builtin_string.cpp
@@ -8,19 +8,19 @@
 
 namespace das
 {
-    bool builtin_string_endswith ( const char * str, const char * cmp, Context * context ) {
+    inline bool builtin_string_endswith ( const char * str, const char * cmp, Context * context ) {
         const uint32_t strLen = stringLengthSafe ( *context, str );
         const uint32_t cmpLen = stringLengthSafe ( *context, cmp );
         return (cmpLen > strLen) ? false : memcmp(&str[strLen - cmpLen], cmp, cmpLen) == 0;
     }
 
-    bool builtin_string_startswith ( const char * str, const char * cmp, Context * context ) {
+    inline bool builtin_string_startswith ( const char * str, const char * cmp, Context * context ) {
         const uint32_t strLen = stringLengthSafe ( *context, str );
         const uint32_t cmpLen = stringLengthSafe ( *context, cmp );
         return (cmpLen > strLen) ? false : memcmp(str, cmp, cmpLen) == 0;
     }
 
-    static const char* strip_l(const char *str)
+    static inline const char* strip_l(const char *str)
     {
         const char *t = str;
         while (((*t) != '\0') && isspace(*t))
@@ -28,7 +28,7 @@ namespace das
         return t;
     }
 
-    static const char* strip_r(const char *str, uint32_t len)
+    static inline const char* strip_r(const char *str, uint32_t len)
     {
         if (len == 0)
             return str;
@@ -38,7 +38,7 @@ namespace das
         return t + 1;
     }
 
-    char* builtin_string_strip ( const char *str, Context * context )
+    inline char* builtin_string_strip ( const char *str, Context * context )
     {
         const uint32_t strLen = stringLengthSafe ( *context, str );
         if (!strLen)
@@ -47,7 +47,7 @@ namespace das
         const char *end = strip_r(str, strLen);
         return end > start ? context->heap.allocateString(start, uint32_t(end-start)) : nullptr;
     }
-    char* builtin_string_strip_left ( const char *str, Context * context )
+    inline char* builtin_string_strip_left ( const char *str, Context * context )
     {
         const uint32_t strLen = stringLengthSafe ( *context, str );
         if (!strLen)
@@ -55,7 +55,7 @@ namespace das
         const char *start = strip_l(str);
         return start-str < strLen ? context->heap.allocateString(start, strLen-uint32_t(start-str)) : nullptr;
     }
-    char* builtin_string_strip_right ( const char *str, Context * context )
+    inline char* builtin_string_strip_right ( const char *str, Context * context )
     {
         const uint32_t strLen = stringLengthSafe ( *context, str );
         if (!strLen)
@@ -69,7 +69,7 @@ namespace das
         return (v < minv) ? minv : (v > maxv) ? maxv : v;
     }
 
-    char* builtin_string_slice1 ( const char *str, int start, int end, Context * context )
+    inline char* builtin_string_slice1 ( const char *str, int start, int end, Context * context )
     {
         const uint32_t strLen = stringLengthSafe ( *context, str );
         if (!strLen)
@@ -78,7 +78,7 @@ namespace das
         end = clamp_int((end < 0) ? (strLen + end) : end, 0, strLen);
         return end > start ? context->heap.allocateString(str + start, uint32_t(end-start)) : nullptr;
     }
-    char* builtin_string_slice2 ( const char *str, int start, Context * context )
+    inline char* builtin_string_slice2 ( const char *str, int start, Context * context )
     {
         const uint32_t strLen = stringLengthSafe ( *context, str );
         if (!strLen)
@@ -86,6 +86,45 @@ namespace das
         start = clamp_int((start < 0) ? (strLen + start) : start, 0, strLen);
         return strLen > uint32_t(start) ? context->heap.allocateString(str + start, uint32_t(strLen-start)) : nullptr;
     }
+
+    inline unsigned string_to_uint ( const char *str, Context * context )
+    {
+        char *endptr;
+        unsigned long int ret = strtoul(str, &endptr, 10);
+        const uint32_t strLen = stringLengthSafe ( *context, str );
+        if (endptr != str + strLen || strLen == 0)
+        {
+            context->throw_error("string-to-uint conversion failed. String is not an uint number");
+            return 0;
+        }
+        return ret;
+    }
+    inline int string_to_int ( const char *str, Context * context )
+    {
+        char *endptr;
+        long int ret = strtol(str, &endptr, 10);
+        const uint32_t strLen = stringLengthSafe ( *context, str );
+        if (endptr != str + strLen || strLen == 0)
+        {
+            context->throw_error("string-to-int conversion failed. String is not an integer number");
+            return 0;
+        }
+        return ret;
+    }
+    inline float string_to_float ( const char *str, Context * context )
+    {
+        char *endptr;
+        float ret = strtof(str, &endptr);
+        const uint32_t strLen = stringLengthSafe ( *context, str );
+        if (endptr != str + strLen || strLen == 0)
+        {
+            context->throw_error("string-to-float conversion failed. String is not an float number");
+            return 0.f;
+        }
+        return ret;
+    }
+    inline float fast_to_float ( const char *str ){return str ? (float)atof(str) : 0.f;}
+    inline int fast_to_int ( const char *str ){return str ? atoi(str) : 0;}
 
     void Module_BuiltIn::addString(ModuleLibrary & lib) {
         addExtern<DAS_BIND_FUN(builtin_string_endswith)>(*this, lib, "endswith", false);
@@ -95,5 +134,10 @@ namespace das
         addExtern<DAS_BIND_FUN(builtin_string_strip_left)>(*this, lib, "strip_left", false);
         addExtern<DAS_BIND_FUN(builtin_string_slice1)>(*this, lib, "slice", false);
         addExtern<DAS_BIND_FUN(builtin_string_slice2)>(*this, lib, "slice", false);
+        addExtern<DAS_BIND_FUN(string_to_int)>(*this, lib, "int", false);
+        addExtern<DAS_BIND_FUN(string_to_uint)>(*this, lib, "uint", false);
+        addExtern<DAS_BIND_FUN(string_to_float)>(*this, lib, "float", false);
+        addExtern<DAS_BIND_FUN(fast_to_int)>(*this, lib, "to_int", false);
+        addExtern<DAS_BIND_FUN(fast_to_float)>(*this, lib, "to_float", false);
     }
 }
