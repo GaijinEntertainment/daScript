532d24c8a6c6bb574ff2dcde535fb55f673dc1c0
Boris Batkin <bbatkin@gmail.com>
2019-02-15 18:02:06 -0800

    move semantic cleanup back to good c-tor

--- src/ast/ast_generate.cpp
+++ src/ast/ast_generate.cpp
@@ -10,37 +10,18 @@ namespace das {
         return del;
     }
 
+    // return [[t()]]
     FunctionPtr makeConstructor ( Structure * str ) {
         auto fn = make_shared<Function>();
         fn->name = str->name;
         fn->at = str->at;
         fn->result = make_shared<TypeDecl>(str->shared_from_this());
         auto block = make_shared<ExprBlock>();
-        // let t : STRUCT_TYPE
-        auto varDecl = make_shared<ExprLet>();
-        varDecl->scoped = false;
-        auto varT = make_shared<Variable>();
-        varT->name = "t";
-        varT->type = make_shared<TypeDecl>(str->shared_from_this());
-        varDecl->variables.push_back(varT);
-        block->list.push_back(varDecl);
-        // for each initialized field
-        for ( auto & fd : str->fields ) {
-            if ( fd.init ) {
-                // t.field = init
-                auto getT = make_shared<ExprVar>(fd.at,"t");
-                auto getTField = make_shared<ExprField>(fd.at,getT,fd.name);
-                shared_ptr<ExprOp2> opEq = fd.type->canCopy() ?
-                static_pointer_cast<ExprOp2>(make_shared<ExprCopy>()) :
-                static_pointer_cast<ExprOp2>(make_shared<ExprMove>());
-                opEq->left = getTField;
-                opEq->right = fd.init->clone();
-                block->list.push_back(opEq);
-            }
-        }
-        // return t
-        auto getT = make_shared<ExprVar>(str->at,"t");
-        auto returnDecl = make_shared<ExprReturn>(str->at,getT);
+        auto makeT = make_shared<ExprMakeStructure>(str->at);
+        makeT->useInitializer = true;
+        makeT->makeType = make_shared<TypeDecl>(str->shared_from_this());
+        makeT->structs.push_back(make_shared<MakeStruct>());
+        auto returnDecl = make_shared<ExprReturn>(str->at,makeT);
         returnDecl->moveSemantics = !str->canCopy();
         block->list.push_back(returnDecl);
         fn->body = block;
