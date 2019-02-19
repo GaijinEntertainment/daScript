745bef2295b68a8139df839524fe7925303d4480
Aliot <anton@gaijin.ru>
2019-02-20 01:26:45 +0300

    squirrel nbodies test added

--- examples/profile/tests/squirrel/run.bat
+++ examples/profile/tests/squirrel/run.bat
@@ -7,6 +7,7 @@ sq3-modified-64.exe native.nut
 sq3-modified-64.exe profile_try_catch.nut 
 sq3-modified-64.exe dict.nut
 sq3-modified-64.exe exp.nut
+sq3-modified-64.exe nbodies.nut
 
 echo "Squirrel3 original"
 sq3-64.exe fib.nut
@@ -14,4 +15,5 @@ sq3-64.exe primes.nut
 sq3-64.exe particles.nut
 sq3-64.exe dict.nut
 sq3-64.exe exp.nut
+sq3-64.exe nbodies.nut
 rem sq3-64.exe native.nut
