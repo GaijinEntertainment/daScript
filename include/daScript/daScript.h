d9f78d176093b6e1eaf2c6627da7de987e55fb2e
Boris Batkin <bbatkin@gmail.com>
2019-04-10 14:25:18 -0700

    proper fix

--- include/daScript/daScript.h
+++ include/daScript/daScript.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <daScript/daScriptModule.h>
 #include <daScript/misc/platform.h>
+#include <daScript/daScriptModule.h>
 #include <daScript/ast/ast.h>
 #include <daScript/ast/ast_interop.h>
 #include <daScript/ast/ast_handle.h>
