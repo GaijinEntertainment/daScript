6d2b8fee9c73fab2bb39e5eb434a304c3c7a3a40
Boris Batkin <bbatkin@gmail.com>
2019-08-23 16:17:21 -0700

    missing fusion

--- include/daScript/simulate/simulate_fusion_op2set.h
+++ include/daScript/simulate/simulate_fusion_op2set.h
@@ -40,19 +40,33 @@
     struct FusionPoint_Set_##OPNAME##_##CTYPE : FusionPointOp2 { \
         IMPLEMENT_OP2_SET_NODE(OPNAME,TYPE,CTYPE,Local,Const); \
         IMPLEMENT_OP2_SET_NODE(OPNAME,TYPE,CTYPE,Local,Local); \
+        IMPLEMENT_OP2_SET_NODE(OPNAME,TYPE,CTYPE,LocalRefOff,LocalRefOff); \
+        IMPLEMENT_OP2_SET_NODE(OPNAME,TYPE,CTYPE,LocalRefOff,Argument); \
+        IMPLEMENT_OP2_SET_NODE(OPNAME,TYPE,CTYPE,ArgumentRefOff,ArgumentRefOff); \
+        IMPLEMENT_OP2_SET_NODE(OPNAME,TYPE,CTYPE,ThisBlockArgumentRef,ThisBlockArgument); \
+        IMPLEMENT_OP2_SET_NODE(OPNAME,TYPE,CTYPE,ThisBlockArgumentRef,ThisBlockArgumentRef); \
         IMPLEMENT_OP2_SET_NODE_ANY(OPNAME,TYPE,CTYPE,AnyPtr); \
         IMPLEMENT_OP2_SET_NODE_ANY(OPNAME,TYPE,CTYPE,Local); \
         IMPLEMENT_OP2_SET_NODE_ANY(OPNAME,TYPE,CTYPE,CMResOfs); \
         IMPLEMENT_OP2_SET_NODE_ANY(OPNAME,TYPE,CTYPE,LocalRefOff); \
         IMPLEMENT_OP2_SET_NODE_ANY(OPNAME,TYPE,CTYPE,ArgumentRef); \
+        IMPLEMENT_OP2_SET_NODE_ANY(OPNAME,TYPE,CTYPE,ArgumentRefOff); \
+        IMPLEMENT_OP2_SET_NODE_ANY(OPNAME,TYPE,CTYPE,ThisBlockArgumentRef); \
         virtual SimNode * match(const SimNodeInfoLookup & info, SimNode *, SimNode * node_l, SimNode * node_r, Context * context) override { \
             if ( false ) {} \
             MATCH_OP2_SET(OPNAME,"GetLocal","ConstValue",Local,Const) \
             MATCH_OP2_SET(OPNAME,"GetLocal","GetLocalR2V",Local,Local) \
+            MATCH_OP2_SET(OPNAME,"GetLocalRefOff","GetLocalRefOffR2V",LocalRefOff,LocalRefOff) \
+            MATCH_OP2_SET(OPNAME,"GetLocalRefOff","GetArgument",LocalRefOff,Argument) \
+            MATCH_OP2_SET(OPNAME,"GetArgumentRefOff","GetArgumentRefOffR2V",ArgumentRefOff,ArgumentRefOff) \
+            MATCH_OP2_SET(OPNAME,"GetThisBlockArgument","GetThisBlockArgument",ThisBlockArgumentRef,ThisBlockArgument) \
+            MATCH_OP2_SET(OPNAME,"GetThisBlockArgument","GetThisBlockArgumentR2V",ThisBlockArgumentRef,ThisBlockArgumentRef) \
             MATCH_OP2_SET_ANY(OPNAME,"GetLocal",Local) \
             MATCH_OP2_SET_ANY(OPNAME,"GetCMResOfs",CMResOfs) \
             MATCH_OP2_SET_ANY(OPNAME,"GetLocalRefOff",LocalRefOff) \
             MATCH_OP2_SET_ANY(OPNAME,"GetArgument",ArgumentRef) \
+            MATCH_OP2_SET_ANY(OPNAME,"GetArgumentRefOff",ArgumentRefOff) \
+            MATCH_OP2_SET_ANY(OPNAME,"GetThisBlockArgument",ThisBlockArgumentRef) \
             return nullptr; \
         } \
         virtual void set(SimNode_Op2Fusion * result, SimNode * node) override { \
