ab2c0cbe34f3443da4f9cd35bf71292caa2d613e
Boris Batkin <bbatkin@gmail.com>
2020-07-03 10:32:00 -0700

    accidently

--- doc/make_site.bat
+++ doc/make_site.bat
@@ -1,2 +1,1 @@
-set SPHINXBUILD="C:\Users\Boris\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.8_qbz5n2kfra8p0\LocalCache\local-packages\Python38\Scripts\sphinx-build.exe"
 call make.bat site
