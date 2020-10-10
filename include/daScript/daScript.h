3e431236b38368a5b220e0ac25a080e4581099f2
Boris Batkin <bbatkin@gmail.com>
2020-10-09 19:14:13 -0700

    first tutorial

--- include/daScript/daScript.h
+++ include/daScript/daScript.h
@@ -6,3 +6,5 @@
 #include <daScript/ast/ast_interop.h>
 #include <daScript/ast/ast_handle.h>
 #include <daScript/simulate/bind_enum.h>
+#include <daScript/simulate/fs_file_info.h>
+#include <daScript/misc/sysos.h>
--- include/daScript/daScriptModule.h
+++ include/daScript/daScriptModule.h
@@ -8,3 +8,15 @@ namespace das
 #define NEED_MODULE(ClassName) \
     extern das::Module * register_##ClassName (); \
     das::ModuleKarma += unsigned(intptr_t(register_##ClassName()));
+
+#define NEED_ALL_DEFAULT_MODULES \
+    NEED_MODULE(Module_BuiltIn); \
+    NEED_MODULE(Module_Math); \
+    NEED_MODULE(Module_Strings); \
+    NEED_MODULE(Module_Rtti); \
+    NEED_MODULE(Module_Ast); \
+    NEED_MODULE(Module_Debugger); \
+    NEED_MODULE(Module_FIO); \
+    NEED_MODULE(Module_Random); \
+    NEED_MODULE(Module_Network);
+
