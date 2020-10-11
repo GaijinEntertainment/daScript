0fd2e30e7e2ae05dd501154e5190214cd2f59afe
Boris Batkin <bbatkin@gmail.com>
2020-10-10 22:03:39 -0700

    nano-tutorial just became smaller

--- examples/tutorial/tutorial00.cpp
+++ examples/tutorial/tutorial00.cpp
@@ -1,8 +1,6 @@
 #include "daScript/daScript.h"
 #include "msvc32.inc"
 
-#define TUTORIAL_NAME   "dummy.das"
-
 const char * tutorial_text = R""""(
 [export]
 def test
@@ -11,27 +9,22 @@ def test
 
 using namespace das;
 
-void tutorial () {
+int main( int, char * [] ) {
+    // request all da-script built in modules
+    NEED_ALL_DEFAULT_MODULES
     // make file access, introduce string as if it was a file
     auto fAccess = make_smart<FsFileAccess>();
     auto fileInfo = make_unique<FileInfo>(tutorial_text, uint32_t(strlen(tutorial_text)));
-    fAccess->setFileInfo(TUTORIAL_NAME, move(fileInfo));
+    fAccess->setFileInfo("dummy.das", move(fileInfo));
     // compile script
     TextPrinter tout;
     ModuleGroup dummyLibGroup;
-    auto program = compileDaScript(TUTORIAL_NAME, fAccess, tout, dummyLibGroup);
+    auto program = compileDaScript("dummy.das", fAccess, tout, dummyLibGroup);
     // create context
     Context ctx(program->getContextStackSize());
     program->simulate(ctx, tout);
     // call context function
     ctx.eval(ctx.findFunction("test"), nullptr);
-}
-
-int main( int, char * [] ) {
-    // request all da-script built in modules
-    NEED_ALL_DEFAULT_MODULES
-    // run the tutorial
-    tutorial();
     // shut-down daScript, free all memory
     Module::Shutdown();
     return 0;
