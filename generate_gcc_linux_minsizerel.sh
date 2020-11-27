f4495d9495ac8fb5fbe701156e8f219ae5a1c82a
Boris Batkin <bbatkin@gmail.com>
2020-11-27 08:57:51 -0800

    more helpers

--- generate_gcc_linux_minsizerel.sh
+++ generate_gcc_linux_minsizerel.sh
@@ -1,5 +1,5 @@
 rm -r -f cmake_temp
 mkdir cmake_temp
 cd cmake_temp
-CC=gcc-9 CXX=g++-9 cmake -DCMAKE_OSX_ARCHITECTURES="x86_64" -DCMAKE_INSTALL_PREFIX=. -DCMAKE_BUILD_TYPE=MINSIZEREL -DCMAKE_OSX_DEPLOYMENT_TARGET=10.12 -G "Unix Makefiles" ../
+CC=gcc-9 CXX=g++-9 cmake -DCMAKE_OSX_ARCHITECTURES="x86_64" -DCMAKE_INSTALL_PREFIX=. -DCMAKE_BUILD_TYPE=MINSIZEREL -G "Unix Makefiles" ../
 
