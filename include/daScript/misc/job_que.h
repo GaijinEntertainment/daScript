ac4c0d889c38088bed3a975f4626da1f89ed78d3
Boris Batkin <bbatkin@gmail.com>
2021-01-11 15:51:15 -0800

    tabs and spaces

--- include/daScript/misc/job_que.h
+++ include/daScript/misc/job_que.h
@@ -7,11 +7,11 @@
 #include <atomic>
 
 namespace das {
-	// single job
+    // single job
     typedef function<void()> Job;
     typedef function<void(int,int)> JobChunk;
 
-	typedef uint32_t JobCategory;
+    typedef uint32_t JobCategory;
 
     enum class JobPriority : int32_t {
         Inactive = 0x66,                    // just some high number to indicate the thread is inactive
@@ -24,84 +24,84 @@ namespace das {
         Realtime = High,
     };
 
-	class JobStatus {
-	public:
-		JobStatus() {};
-		JobStatus(uint32_t count) { Clear( count); };
-		JobStatus ( JobStatus && ) = delete;
-		JobStatus ( const JobStatus & ) = delete;
-		JobStatus & operator = ( JobStatus && ) = delete;
-		JobStatus & operator = ( const JobStatus & ) = delete;
-		void Notify();
-		bool IsReady();
-		void Wait();
-		void Clear(uint32_t count = 1);
-	protected:
-		mutex				mCompleteMutex;
-		uint32_t			mRemaining = 0;
-		condition_variable	mCond;
-	};
+    class JobStatus {
+    public:
+        JobStatus() {};
+        JobStatus(uint32_t count) { Clear( count); };
+        JobStatus ( JobStatus && ) = delete;
+        JobStatus ( const JobStatus & ) = delete;
+        JobStatus & operator = ( JobStatus && ) = delete;
+        JobStatus & operator = ( const JobStatus & ) = delete;
+        void Notify();
+        bool IsReady();
+        void Wait();
+        void Clear(uint32_t count = 1);
+    protected:
+        mutex				mCompleteMutex;
+        uint32_t			mRemaining = 0;
+        condition_variable	mCond;
+    };
 
     class JobQue {
     public:
-		JobQue();
+        JobQue();
         JobQue ( const JobQue & ) = delete;
         JobQue ( JobQue && ) = delete;
         JobQue & operator = ( const JobQue & ) = delete;
         JobQue & operator = ( JobQue && ) = delete;
         ~JobQue ();
         bool isEmpty (bool includingMainThreadJobs = false);
-		bool areJobsPending(JobCategory category);
-		int getNumberOfQueuedJobs();
-		int getTotalHwJobs();
-		void push(Job && job, JobCategory category, JobPriority priority);
+        bool areJobsPending(JobCategory category);
+        int getNumberOfQueuedJobs();
+        int getTotalHwJobs();
+        void push(Job && job, JobCategory category, JobPriority priority);
         void parallel_for ( JobStatus & status, int from, int to, const JobChunk & chunk, JobCategory category, JobPriority priority, int chunk_count = -1, int step = 1 );
         void parallel_for ( int from, int to, const JobChunk & chunk, JobCategory category, JobPriority priority, int chunk_count = -1, int step = 1 );
-		void parallel_for_with_consume (int from, int to, const JobChunk & chunk, const JobChunk & consume, JobCategory category, JobPriority priority, int chunk_count = -1, int step = 1);
-		static int get_num_threads() { return max(1,static_cast<int>(thread::hardware_concurrency())); }
-		void EvalOnMainThread(Job && expr);
-		void EvalMainThreadJobs();
-		void wait();
-		void Reset() { wait( ); }
-	protected:
-		struct JobEntry {
-			JobEntry( Job&& _function, JobCategory _category, JobPriority _priority) {
-				function = move(_function);
-				priority = _priority;
-				category = _category;
-			}
-			Job				function = nullptr;
-			JobPriority		priority = JobPriority::Inactive;
-			JobCategory		category = 0;
-		};
-		struct ThreadEntry {
-			ThreadEntry( thread* thread) {
-				threadPointer.reset(thread);
-			};
-			unique_ptr<thread>	threadPointer;
-			JobPriority			currentPriority = JobPriority::Inactive;
-			JobCategory			currentCategory = 0;
-		};
-	protected:
-		void join();
-		void job(int threadIndex);
-		void submit(Job && job, JobCategory category, JobPriority priority);
-	protected:
-		condition_variable mCond;
-		int mSleepMs;
-		atomic<bool>	mShutdown;
-		atomic<int>		mThreadCount;
-		static thread::id mTheMainThread;
-		mutex mFifoMutex;
+        void parallel_for_with_consume (int from, int to, const JobChunk & chunk, const JobChunk & consume, JobCategory category, JobPriority priority, int chunk_count = -1, int step = 1);
+        static int get_num_threads() { return max(1,static_cast<int>(thread::hardware_concurrency())); }
+        void EvalOnMainThread(Job && expr);
+        void EvalMainThreadJobs();
+        void wait();
+        void Reset() { wait( ); }
+    protected:
+        struct JobEntry {
+            JobEntry( Job&& _function, JobCategory _category, JobPriority _priority) {
+                function = move(_function);
+                priority = _priority;
+                category = _category;
+            }
+            Job				function = nullptr;
+            JobPriority		priority = JobPriority::Inactive;
+            JobCategory		category = 0;
+        };
+        struct ThreadEntry {
+            ThreadEntry( thread* thread) {
+                threadPointer.reset(thread);
+            };
+            unique_ptr<thread>	threadPointer;
+            JobPriority			currentPriority = JobPriority::Inactive;
+            JobCategory			currentCategory = 0;
+        };
+    protected:
+        void join();
+        void job(int threadIndex);
+        void submit(Job && job, JobCategory category, JobPriority priority);
+    protected:
+        condition_variable mCond;
+        int mSleepMs;
+        atomic<bool>	mShutdown;
+        atomic<int>		mThreadCount;
+        static thread::id mTheMainThread;
+        mutex mFifoMutex;
     protected:
         deque<JobEntry>	mFifo;
         vector<ThreadEntry>		mThreads;
         atomic<int> mJobsRunning;
-	protected:
-		mutex mEvalMainThreadMutex;
-		vector<Job> mEvalMainThread;
+    protected:
+        mutex mEvalMainThreadMutex;
+        vector<Job> mEvalMainThread;
     };
 
     void SetCurrentThreadName ( const string & str );
-	void SetCurrentThreadPriority ( JobPriority priority );             // change priority of current thread, 0 - normal, >0 above normal, <0 below normal
+    void SetCurrentThreadPriority ( JobPriority priority );             // change priority of current thread, 0 - normal, >0 above normal, <0 below normal
 }
--- src/misc/job_que.cpp
+++ src/misc/job_que.cpp
@@ -4,132 +4,132 @@
 
 namespace das {
 
-	JobQue::JobQue()
-		: mSleepMs(0)
-		, mShutdown(false)
-		, mThreadCount( 0 )
-		, mJobsRunning(0) {
-		mThreadCount = max(1,(static_cast<int>(thread::hardware_concurrency())));
-		SetCurrentThreadPriority(JobPriority::High);
-		for (int j = 0; j < mThreadCount; j++) {
-			mThreads.emplace_back(new thread([=]() {
-				string thread_name = "JobQue_Job_" + to_string(j);
-				SetCurrentThreadName(thread_name);
-				job(j);
-			}));
-		}
-	}
+    JobQue::JobQue()
+        : mSleepMs(0)
+        , mShutdown(false)
+        , mThreadCount( 0 )
+        , mJobsRunning(0) {
+        mThreadCount = max(1,(static_cast<int>(thread::hardware_concurrency())));
+        SetCurrentThreadPriority(JobPriority::High);
+        for (int j = 0; j < mThreadCount; j++) {
+            mThreads.emplace_back(new thread([=]() {
+                string thread_name = "JobQue_Job_" + to_string(j);
+                SetCurrentThreadName(thread_name);
+                job(j);
+            }));
+        }
+    }
 
     JobQue::~JobQue () {
         join();
     }
 
-	void JobQue::EvalOnMainThread(Job && expr) {
-		lock_guard<mutex> guard(mEvalMainThreadMutex);
-		mEvalMainThread.emplace_back(move(expr));
-	}
+    void JobQue::EvalOnMainThread(Job && expr) {
+        lock_guard<mutex> guard(mEvalMainThreadMutex);
+        mEvalMainThread.emplace_back(move(expr));
+    }
 
-	void JobQue::EvalMainThreadJobs() {
-		vector<Job> results;
-		{
-			lock_guard<mutex> guard(mEvalMainThreadMutex);
-			swap(results, mEvalMainThread);
-		}
-		for (auto & job : results) {
-			job();
+    void JobQue::EvalMainThreadJobs() {
+        vector<Job> results;
+        {
+            lock_guard<mutex> guard(mEvalMainThreadMutex);
+            swap(results, mEvalMainThread);
         }
-	}
+        for (auto & job : results) {
+            job();
+        }
+    }
 
-	void JobQue::wait() {
-		while ( !isEmpty(true) ) {
-			while ( !isEmpty() ) {
-				this_thread::yield();
+    void JobQue::wait() {
+        while ( !isEmpty(true) ) {
+            while ( !isEmpty() ) {
+                this_thread::yield();
             }
-			EvalMainThreadJobs();
-		}
-	}
+            EvalMainThreadJobs();
+        }
+    }
 
     void JobQue::join() {
         mShutdown = true;
         while ( mThreadCount ) {
             this_thread::yield();
         }
-		for (auto & th : mThreads) {
-			th.threadPointer->join();
+        for (auto & th : mThreads) {
+            th.threadPointer->join();
         }
-		mThreads.clear();
-	}
+        mThreads.clear();
+    }
 
     bool JobQue::isEmpty ( bool includingMainThreadJobs ) {
         lock_guard<mutex> lock(mFifoMutex);
-		bool queue_is_empty = (mJobsRunning == 0) && (mFifo.size() == 0);
-		if ( includingMainThreadJobs ) {
-			lock_guard<mutex> mainThreadLock(mEvalMainThreadMutex);
-			return queue_is_empty && mEvalMainThread.empty();
-		}
-		return queue_is_empty;
+        bool queue_is_empty = (mJobsRunning == 0) && (mFifo.size() == 0);
+        if ( includingMainThreadJobs ) {
+            lock_guard<mutex> mainThreadLock(mEvalMainThreadMutex);
+            return queue_is_empty && mEvalMainThread.empty();
+        }
+        return queue_is_empty;
     }
 
-	bool JobQue::areJobsPending(JobCategory category) {
+    bool JobQue::areJobsPending(JobCategory category) {
         lock_guard<mutex> lock(mFifoMutex);
-		if (find_if(mFifo.begin(), mFifo.end(), [=](const JobEntry& jobEntry) {
+        if (find_if(mFifo.begin(), mFifo.end(), [=](const JobEntry& jobEntry) {
                 return jobEntry.category == category; }) != mFifo.end()) {
-			return true;
+            return true;
         }
-		if (find_if(mThreads.begin(), mThreads.end(), [=](const ThreadEntry& threadEntry) {
+        if (find_if(mThreads.begin(), mThreads.end(), [=](const ThreadEntry& threadEntry) {
                 return threadEntry.currentPriority != JobPriority::Inactive && threadEntry.currentCategory == category; }) != mThreads.end()) {
-			return true;
+            return true;
         }
-		return false;
-	}
-
-	int JobQue::getTotalHwJobs() {
-		return mThreadCount;
-	}
-
-	int JobQue::getNumberOfQueuedJobs() {
-		lock_guard<mutex> lock(mFifoMutex);
-		return int(mFifo.size());
-	}
-
-	void JobQue::submit(Job && job, JobCategory category, JobPriority priority) {
-		auto  it = lower_bound(mFifo.begin(), mFifo.end(), priority, [](const JobEntry& lhs, JobPriority priority) {
-			return lhs.priority >= priority; });
-		mFifo.emplace(it, move(job), category, priority);
-	}
-
-	void JobQue::push(Job && job, JobCategory category, JobPriority priority) {
-		lock_guard<mutex> lock(mFifoMutex);
-		submit(move(job), category, priority);
-		mCond.notify_one();
+        return false;
     }
 
-	void JobQue::job(int threadIndex) {
-		while (!mShutdown) {
-			Job job;
-			{
-				unique_lock<mutex> lock(mFifoMutex);
-				if ( mCond.wait_for(lock, chrono::milliseconds(mSleepMs), [&]() { return mFifo.size() != 0; }) ) {
-					DAS_ASSERTF(mFifo.size() > 0, "There must be at least one job available");
-					job = move(mFifo.front().function);
-					mThreads[threadIndex].currentPriority = mFifo.front().priority;
-					mThreads[threadIndex].currentCategory = mFifo.front().category;
-					mFifo.pop_front();
-					mJobsRunning++;
-				} else {
-					continue;
+    int JobQue::getTotalHwJobs() {
+        return mThreadCount;
+    }
+
+    int JobQue::getNumberOfQueuedJobs() {
+        lock_guard<mutex> lock(mFifoMutex);
+        return int(mFifo.size());
+    }
+
+    void JobQue::submit(Job && job, JobCategory category, JobPriority priority) {
+        auto  it = lower_bound(mFifo.begin(), mFifo.end(), priority, [](const JobEntry& lhs, JobPriority priority) {
+            return lhs.priority >= priority; });
+        mFifo.emplace(it, move(job), category, priority);
+    }
+
+    void JobQue::push(Job && job, JobCategory category, JobPriority priority) {
+        lock_guard<mutex> lock(mFifoMutex);
+        submit(move(job), category, priority);
+        mCond.notify_one();
+    }
+
+    void JobQue::job(int threadIndex) {
+        while (!mShutdown) {
+            Job job;
+            {
+                unique_lock<mutex> lock(mFifoMutex);
+                if ( mCond.wait_for(lock, chrono::milliseconds(mSleepMs), [&]() { return mFifo.size() != 0; }) ) {
+                    DAS_ASSERTF(mFifo.size() > 0, "There must be at least one job available");
+                    job = move(mFifo.front().function);
+                    mThreads[threadIndex].currentPriority = mFifo.front().priority;
+                    mThreads[threadIndex].currentCategory = mFifo.front().category;
+                    mFifo.pop_front();
+                    mJobsRunning++;
+                } else {
+                    continue;
                 }
-			}
-			SetCurrentThreadPriority(mThreads[threadIndex].currentPriority);
-			job();
-			{
-				unique_lock<mutex> lock(mFifoMutex);
-				mThreads[threadIndex].currentPriority = JobPriority::Inactive;
-				mJobsRunning--;
-			}
-		}
-		mThreadCount--;
-	}
+            }
+            SetCurrentThreadPriority(mThreads[threadIndex].currentPriority);
+            job();
+            {
+                unique_lock<mutex> lock(mFifoMutex);
+                mThreads[threadIndex].currentPriority = JobPriority::Inactive;
+                mJobsRunning--;
+            }
+        }
+        mThreadCount--;
+    }
 
     void JobQue::parallel_for ( JobStatus & status, int from, int to, const JobChunk & chunk,
             JobCategory category, JobPriority priority, int chunk_count, int step ) {
@@ -144,18 +144,18 @@ namespace das {
         int onMainThread = max ( (numChunks + mThreadCount)  / (mThreadCount+1), 1 );
         int onThreads  = numChunks - onMainThread;
         status.Clear(onThreads);
-		{
-			lock_guard<mutex> lock(mFifoMutex);
-			for (int ch = 0; ch < onThreads; ++ch) {
-	            int i0 = from + ch * step;
-	            int i1 = i0 + step;
-	            submit([=,&status](){
-	                chunk(i0, i1);
-	                status.Notify();
-	            }, category, priority);
-	        }
-			mCond.notify_all();
-		}
+        {
+            lock_guard<mutex> lock(mFifoMutex);
+            for (int ch = 0; ch < onThreads; ++ch) {
+                int i0 = from + ch * step;
+                int i1 = i0 + step;
+                submit([=,&status](){
+                    chunk(i0, i1);
+                    status.Notify();
+                }, category, priority);
+            }
+            mCond.notify_all();
+        }
         chunk(from + onThreads * step, to);
     }
 
@@ -165,81 +165,81 @@ namespace das {
         status.Wait();
     }
 
-	void JobQue::parallel_for_with_consume(int from, int to, const JobChunk & chunk, const JobChunk & consume,
+    void JobQue::parallel_for_with_consume(int from, int to, const JobChunk & chunk, const JobChunk & consume,
         JobCategory category, JobPriority priority, int chunk_count, int step) {
-		if (from >= to) return;
-		if (chunk_count == -1) chunk_count = mThreadCount * 4;
-		step = max((to - from) / chunk_count, step);
-		int numChunks = (to - from) / step;
-		if (numChunks == 1) {
-			chunk(from, to);
-			consume(from, to);
-			return;
-		}
-		deque<Job> producerFifoJobs;
-		mutex producerFifoMutex;
-		condition_variable condition;
-		{
-			lock_guard<mutex> lock(mFifoMutex);
-			for (int ch = 0; ch < numChunks; ++ch) {
-	            int i0 = from + ch * step;
-	            int i1 = min(i0 + step, to);
-	            submit([=, &chunk, &producerFifoJobs, &producerFifoMutex, &condition]() {
-	                chunk(i0, i1);
-	                {
+        if (from >= to) return;
+        if (chunk_count == -1) chunk_count = mThreadCount * 4;
+        step = max((to - from) / chunk_count, step);
+        int numChunks = (to - from) / step;
+        if (numChunks == 1) {
+            chunk(from, to);
+            consume(from, to);
+            return;
+        }
+        deque<Job> producerFifoJobs;
+        mutex producerFifoMutex;
+        condition_variable condition;
+        {
+            lock_guard<mutex> lock(mFifoMutex);
+            for (int ch = 0; ch < numChunks; ++ch) {
+                int i0 = from + ch * step;
+                int i1 = min(i0 + step, to);
+                submit([=, &chunk, &producerFifoJobs, &producerFifoMutex, &condition]() {
+                    chunk(i0, i1);
+                    {
                         lock_guard<mutex> producerFifoLock(producerFifoMutex);
                         producerFifoJobs.push_back(([=]() { consume(i0, i1); }));
                         condition.notify_one();
-	                }
-	            }, category, priority);
-			}
-			mCond.notify_all();
-		}
-		{
-			int chunksRemaining = numChunks;
-			while (chunksRemaining > 0) {
-				deque<Job> consumerFifoJobs;
-				{
-					unique_lock<mutex> producerFifoLock(producerFifoMutex);
-					if (condition.wait_for(producerFifoLock, chrono::milliseconds(mSleepMs), [&]() {return producerFifoJobs.size() > 0; })) {
-						consumerFifoJobs.swap(producerFifoJobs);
-					} else {
-						continue;
                     }
-				}
-				for (auto & job : consumerFifoJobs) {
-					job();
-					--chunksRemaining;
-				}
-			}
-		}
-	}
+                }, category, priority);
+            }
+            mCond.notify_all();
+        }
+        {
+            int chunksRemaining = numChunks;
+            while (chunksRemaining > 0) {
+                deque<Job> consumerFifoJobs;
+                {
+                    unique_lock<mutex> producerFifoLock(producerFifoMutex);
+                    if (condition.wait_for(producerFifoLock, chrono::milliseconds(mSleepMs), [&]() {return producerFifoJobs.size() > 0; })) {
+                        consumerFifoJobs.swap(producerFifoJobs);
+                    } else {
+                        continue;
+                    }
+                }
+                for (auto & job : consumerFifoJobs) {
+                    job();
+                    --chunksRemaining;
+                }
+            }
+        }
+    }
 
-	void JobStatus::Notify() {
-		lock_guard<mutex> guard(mCompleteMutex);
+    void JobStatus::Notify() {
+        lock_guard<mutex> guard(mCompleteMutex);
         DAS_ASSERTF(mRemaining != 0, "Nothing to notify!");
         --mRemaining;
         if ( mRemaining==0 ) {
             mCond.notify_all();
         }
-	}
+    }
 
-	void JobStatus::Wait() {
+    void JobStatus::Wait() {
         unique_lock<mutex> lock(mCompleteMutex);
         while ( mRemaining ) {
             mCond.wait(lock);
         }
-	}
+    }
 
-	bool JobStatus::IsReady() {
+    bool JobStatus::IsReady() {
         lock_guard<mutex> guard(mCompleteMutex);
         return mRemaining==0;
-	}
+    }
 
-	void JobStatus::Clear(uint32_t count) {
+    void JobStatus::Clear(uint32_t count) {
         lock_guard<mutex> guard(mCompleteMutex);
         mRemaining = count;
-	}
+    }
 }
 
 #if defined(__APPLE__)
@@ -249,15 +249,15 @@ namespace das {
         pthread_setname_np(str.c_str());
     }
 
-	void SetCurrentThreadPriority ( JobPriority _priority ) {
+    void SetCurrentThreadPriority ( JobPriority _priority ) {
         int priority = int(_priority);
-		float minPlatformPriority = sched_get_priority_min(SCHED_OTHER);
-		float maxPlatformPriority = sched_get_priority_max(SCHED_OTHER);
-		struct sched_param sched_param;
-		int platformPriority = (int)(minPlatformPriority + (maxPlatformPriority - minPlatformPriority)
-			* ((float)(priority - int(JobPriority::Minimum)) / (float)(int(JobPriority::Maximum) - int(JobPriority::Minimum))));
-		sched_param.sched_priority = platformPriority;
-		pthread_setschedparam(pthread_self(), SCHED_OTHER, &sched_param);
+        float minPlatformPriority = sched_get_priority_min(SCHED_OTHER);
+        float maxPlatformPriority = sched_get_priority_max(SCHED_OTHER);
+        struct sched_param sched_param;
+        int platformPriority = (int)(minPlatformPriority + (maxPlatformPriority - minPlatformPriority)
+            * ((float)(priority - int(JobPriority::Minimum)) / (float)(int(JobPriority::Maximum) - int(JobPriority::Minimum))));
+        sched_param.sched_priority = platformPriority;
+        pthread_setschedparam(pthread_self(), SCHED_OTHER, &sched_param);
     }
 }
 
@@ -290,20 +290,20 @@ namespace das {
             RaiseException( MS_VC_EXCEPTION, 0, sizeof(info)/sizeof(ULONG_PTR), (ULONG_PTR*)&info );
         } __except(EXCEPTION_EXECUTE_HANDLER) {
         }
-	}
+    }
 
-	void SetCurrentThreadPriority ( JobPriority priority ) {
- 		int winPriority = THREAD_PRIORITY_NORMAL;
-		switch (priority) {
-		case JobPriority::Minimum:  winPriority = THREAD_PRIORITY_LOWEST; break;
-		case JobPriority::Low:      winPriority = THREAD_PRIORITY_BELOW_NORMAL; break;
-		case JobPriority::Medium:   winPriority = THREAD_PRIORITY_NORMAL; break;
-		case JobPriority::High:     winPriority = THREAD_PRIORITY_ABOVE_NORMAL; break;
-		case JobPriority::Maximum:  winPriority = THREAD_PRIORITY_HIGHEST; break;
-		default:					DAS_ASSERTF(0, "Windows takes prefixed priority values"); break;
-		}
-		SetThreadPriority(GetCurrentThread(), winPriority);
-	}
+    void SetCurrentThreadPriority ( JobPriority priority ) {
+        int winPriority = THREAD_PRIORITY_NORMAL;
+        switch (priority) {
+        case JobPriority::Minimum:  winPriority = THREAD_PRIORITY_LOWEST; break;
+        case JobPriority::Low:      winPriority = THREAD_PRIORITY_BELOW_NORMAL; break;
+        case JobPriority::Medium:   winPriority = THREAD_PRIORITY_NORMAL; break;
+        case JobPriority::High:     winPriority = THREAD_PRIORITY_ABOVE_NORMAL; break;
+        case JobPriority::Maximum:  winPriority = THREAD_PRIORITY_HIGHEST; break;
+        default:					DAS_ASSERTF(0, "Windows takes prefixed priority values"); break;
+        }
+        SetThreadPriority(GetCurrentThread(), winPriority);
+    }
 }
 
 #else
@@ -315,16 +315,16 @@ namespace das
         pthread_setname_np(pthread_self(), str.c_str());
     }
 
-	void SetCurrentThreadPriority ( JobPriority _priority ) {
+    void SetCurrentThreadPriority ( JobPriority _priority ) {
         int priority = int(_priority);
-		float minPlatformPriority = sched_get_priority_min(SCHED_OTHER);
-		float maxPlatformPriority = sched_get_priority_max(SCHED_OTHER);
-		struct sched_param sched_param;
-		int platformPriority = (int)( minPlatformPriority + (maxPlatformPriority - minPlatformPriority)
-			* ((float)(priority - int(JobPriority::Minimum)) / (float)(int(JobPriority::Maximum) - int(JobPriority::Minimum))));
-		sched_param.sched_priority = platformPriority;
-		pthread_setschedparam(pthread_self(), SCHED_OTHER, &sched_param);
-	}
+        float minPlatformPriority = sched_get_priority_min(SCHED_OTHER);
+        float maxPlatformPriority = sched_get_priority_max(SCHED_OTHER);
+        struct sched_param sched_param;
+        int platformPriority = (int)( minPlatformPriority + (maxPlatformPriority - minPlatformPriority)
+            * ((float)(priority - int(JobPriority::Minimum)) / (float)(int(JobPriority::Maximum) - int(JobPriority::Minimum))));
+        sched_param.sched_priority = platformPriority;
+        pthread_setschedparam(pthread_self(), SCHED_OTHER, &sched_param);
+    }
 }
 
 #endif
