5b54b6cd149f11ccd81c97c72cc489d3f1c75aed
Boris Batkin <bbatkin@gmail.com>
2021-07-16 19:32:24 -0700

    do not optimize out var x : int global

--- src/ast/ast_unused.cpp
+++ src/ast/ast_unused.cpp
@@ -550,7 +550,7 @@ namespace das {
                             }
                         }
                     } else {
-                        if ( expr->type->isFoldable() && !expr->variable->access_init ) {
+                        if ( expr->type->isFoldable() && !expr->variable->access_init && expr->variable->type->constant ) {
                             if ( expr->type->isEnumT() ) {
                                 auto cfv = expr->type->enumType->find(0, "");
                                 if ( !cfv.empty() ) {
