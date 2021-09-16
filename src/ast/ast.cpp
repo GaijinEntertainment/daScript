556e7bf20f5dc8e8f81cdcd9e9891c96a6e8e5f8
Boris Batkin <bbatkin@gmail.com>
2021-09-16 10:38:59 -0700

    more descriptive error message

--- src/ast/ast.cpp
+++ src/ast/ast.cpp
@@ -787,9 +787,9 @@ namespace das {
             if (fnX->result->isRefType() && !fnX->result->ref) {
                 DAS_FATAL_LOG(
                     "addExtern(%s)::failed in module %s\n"
-                    "  this function should be bound with SimNode_ExtFuncCallAndCopyOrMove option\n"
+                    "  this function should be bound with addExtern<DAS_BIND_FUNC(%s), SimNode_ExtFuncCallAndCopyOrMove>\n"
                     "  likely cast<> is implemented for the return type, and it should not\n",
-                    fnX->name.c_str(), mod.name.c_str());
+                    fnX->name.c_str(), mod.name.c_str(), fnX->name.c_str());
                 DAS_FATAL_ERROR;
             }
         }
