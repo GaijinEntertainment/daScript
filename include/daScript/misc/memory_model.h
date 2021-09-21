703a9e52684e3a5d8aaf2e88f443517deaed9622
Boris Batkin <bbatkin@gmail.com>
2021-09-21 12:25:48 -0700

    less warnings

--- include/daScript/misc/memory_model.h
+++ include/daScript/misc/memory_model.h
@@ -137,7 +137,7 @@ namespace das {
                     return;
                 }
             }
-            DAS_FATAL_LOG("deleting %p %i, which is not a chunk pointer (or chunk size mismatch)\n", ptr, size);
+            DAS_FATAL_LOG("deleting %p %i, which is not a chunk pointer (or chunk size mismatch)\n", (void *)ptr, size);
             DAS_FATAL_ERROR("not a valid chunk pointer");
         }
         bool mark ( char * ptr, uint32_t size ) {
