f3c7306a780582f104b8dbc0ae09da94134fb429
Boris Batkin <bbatkin@gmail.com>
2022-09-07 13:12:32 -0700

    with jit example

--- modules/dasLLVM/src/dasLLVM.main.cpp
+++ modules/dasLLVM/src/dasLLVM.main.cpp
@@ -15,13 +15,22 @@ LLVMDisasmContextRef das_LLVMCreateDisasm (const char *TripleName, void *DisInfo
     return LLVMCreateDisasm(TripleName,DisInfo,TagType,nullptr,nullptr);
 }
 
+int run_int_int_int_function ( void * pfun, int32_t a1, int32_t a2 ) {
+    int (*fun)(int32_t,int32_t) = (int(*)(int32_t,int32_t)) pfun;
+    return (*fun)(a1,a2);
+}
+
 void Module_dasLLVM::initMain() {
 	addExtern< void (*)(LLVMOpaquePassRegistry *) , LLVMInitializeCore >(*this,lib,"LLVMInitializeCore",SideEffects::worstDefault,"LLVMInitializeCore")
 		->args({"R"});
     // disasm
 	addExtern<DAS_BIND_FUN(das_LLVMCreateDisasm) >(*this,lib,"LLVMCreateDisasm",SideEffects::worstDefault,"das_LLVMCreateDisasm")
 		->args({"TripleName","DisInfo","TagType"});
+    // sample function
+	addExtern<DAS_BIND_FUN(run_int_int_int_function) >(*this,lib,"run_int_int_int_function",SideEffects::worstDefault,"run_int_int_int_function")
+		->args({"pfun","arg1","arg2"});
 }
+
 
 ModuleAotType Module_dasLLVM::aotRequire ( TextWriter & tw ) const {
     // add your stuff here
