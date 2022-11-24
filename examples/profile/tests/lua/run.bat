5db25bfbbfa86a0b974ec6be1c4f7c8301befa8a
Anton Yudintsev <anton@gaijin.team>
2022-11-25 00:57:03 +0200

    table-sort benchmark added
    from https://github.com/luajit-remake/luajit-remake

--- examples/profile/tests/lua/run.bat
+++ examples/profile/tests/lua/run.bat
@@ -9,6 +9,7 @@ luajit2_1_0beta3\luajit.exe -joff exp.lua
 luajit2_1_0beta3\luajit.exe -joff nbodies.lua
 luajit2_1_0beta3\luajit.exe -joff sha1.lua
 luajit2_1_0beta3\luajit.exe -joff tree.lua
+luajit2_1_0beta3\luajit.exe -joff table-sort.lua
 
 echo "LuaJIT, JIT"
 luajit2_1_0beta3\luajit.exe fibI.lua
@@ -21,6 +22,7 @@ luajit2_1_0beta3\luajit.exe exp.lua
 luajit2_1_0beta3\luajit.exe nbodies.lua
 luajit2_1_0beta3\luajit.exe sha1.lua
 luajit2_1_0beta3\luajit.exe tree.lua
+luajit2_1_0beta3\luajit.exe table-sort.lua
 
 echo "Lua (low res timer, no addOne)"
 lua.exe fibI.lua
@@ -32,4 +34,4 @@ lua.exe exp.lua
 lua.exe nbodies.lua
 lua.exe sha1.lua
 lua.exe tree.lua
-
+lua.exe table-sort.lua
