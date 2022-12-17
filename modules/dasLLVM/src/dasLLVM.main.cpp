4508f3ce55559a46a96ab55a3f3746d7c5d39c43
Boris Batkin <bbatkin@gmail.com>
2022-12-17 14:55:51 -0800

    LLVM vector type promotion to pointers

--- modules/dasLLVM/src/dasLLVM.main.cpp
+++ modules/dasLLVM/src/dasLLVM.main.cpp
@@ -15,12 +15,24 @@ LLVMDisasmContextRef das_LLVMCreateDisasm (const char *TripleName, void *DisInfo
     return LLVMCreateDisasm(TripleName,DisInfo,TagType,nullptr,nullptr);
 }
 
+___noinline float3 test_abi_mad ( float3 a, float3 b, float3 c ) {
+    /*
+    printf("a = %f,%f,%f\n", v_extract_x(a), v_extract_y(a), v_extract_z(a));
+    printf("b = %f,%f,%f\n", v_extract_x(b), v_extract_y(b), v_extract_z(b));
+    printf("c = %f,%f,%f\n", v_extract_x(c), v_extract_y(c), v_extract_z(c));
+    */
+    return v_add(v_mul(a,b),c);
+}
+
 void Module_dasLLVM::initMain() {
 	addExtern< void (*)(LLVMOpaquePassRegistry *) , LLVMInitializeCore >(*this,lib,"LLVMInitializeCore",SideEffects::worstDefault,"LLVMInitializeCore")
 		->args({"R"});
     // disasm
 	addExtern<DAS_BIND_FUN(das_LLVMCreateDisasm) >(*this,lib,"LLVMCreateDisasm",SideEffects::worstDefault,"das_LLVMCreateDisasm")
 		->args({"TripleName","DisInfo","TagType"});
+    // abi tests
+	addExtern<DAS_BIND_FUN(test_abi_mad) >(*this,lib,"test_abi_mad",SideEffects::worstDefault,"test_abi_mad")
+		->args({"a","b","c"});
 }
 
 ModuleAotType Module_dasLLVM::aotRequire ( TextWriter & tw ) const {
