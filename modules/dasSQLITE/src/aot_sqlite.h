a5bb04ddbc998875a3af666129b3cd4d2f05d5f2
Boris Batkin <bbatkin@gmail.com>
2023-03-31 11:51:05 -0700

    some positively good api

--- modules/dasSQLITE/src/aot_sqlite.h
+++ modules/dasSQLITE/src/aot_sqlite.h
@@ -2,3 +2,10 @@
 
 #include "cb_dasSQLITE.h"
 #include "need_dasSQLITE.h"
+
+namespace das {
+    int sqlite3_exec(sqlite3 * db, const char * sql, char ** errmsg);
+    int sqlite3_exec_cb(sqlite3 * db, const char * sql, char ** errmsg,
+        const TBlock<int,const TTemporary<TArray<char *>>,const TTemporary<TArray<char *>>> & block,
+            Context * context, LineInfoArg * at );
+}
--- modules/dasSQLITE/src/dasSQLITE.main.cpp
+++ modules/dasSQLITE/src/dasSQLITE.main.cpp
@@ -7,20 +7,90 @@
 
 #include "dasSQLITE.h"
 
-#include "cb_dasSQLITE.h"
+#include "aot_sqlite.h"
 
 namespace das {
 
+#if 0
+SQLITE_API int sqlite3_exec(
+  sqlite3*,                                  /* An open database */
+  const char *sql,                           /* SQL to be evaluated */
+  int (*callback)(void*,int,char**,char**),  /* Callback function */
+  void *,                                    /* 1st argument to callback */
+  char **errmsg                              /* Error msg written here */
+);
+#endif
+
+int sqlite3_exec(sqlite3 * db, const char * sql, char ** errmsg) {
+    return sqlite3_exec(db, sql, nullptr, nullptr, errmsg);
+}
+
+struct ExecCallback {
+    const Block * block = nullptr;
+    Context * context = nullptr;
+    LineInfoArg * at = nullptr;
+};
+
+int exec_callback ( void * arg, int argc, char ** argv, char ** colName ) {
+    ExecCallback * cb = (ExecCallback *) arg;
+    Array arrFields;
+    arrFields.data = (char *) argv;
+    arrFields.capacity = arrFields.size = argc;
+    arrFields.lock = 1;
+    arrFields.flags = 0;
+    Array arrColumns;
+    arrColumns.data = (char *) colName;
+    arrColumns.capacity = arrColumns.size = argc;
+    arrColumns.lock = 1;
+    arrColumns.flags = 0;
+    vec4f args[2] = {
+        cast<Array *>::from(&arrFields),
+        cast<Array *>::from(&arrColumns)
+    };
+    auto res = cb->context->invoke(*cb->block, args, nullptr, cb->at);
+    return cast<int>::to(res);
+}
+
+int sqlite3_exec_cb(sqlite3 * db, const char * sql, char ** errmsg,
+        const TBlock<int,const TTemporary<TArray<char *>>,const TTemporary<TArray<char *>>> & block,
+            Context * context, LineInfoArg * at ) {
+    ExecCallback cb;
+    cb.block = &block;
+    cb.context = context;
+    cb.at = at;
+    return sqlite3_exec(db, sql, exec_callback, &cb, errmsg);
+}
+
 void Module_dasSQLITE::initMain() {
-    // ok, lets fix up everything returning uint8? into returning string
-    functions.foreach([&](const FunctionPtr & fn) {
-        if ( fn->result->isPointer() && fn->result->firstType &&
-                fn->result->firstType->baseType==Type::tUInt8 && fn->result->firstType->dim.size()==0 ) {
-            fn->result = make_smart<TypeDecl>(Type::tString);
-            fn->result->constant = true;
-            fn->result->temporary = true;
+
+    addExtern<DAS_BIND_FUN(sqlite3_exec)>(*this,lib,"sqlite3_exec",
+        SideEffects::worstDefault, "sqlite3_exec")
+            ->args({"db","sql","errmsg"});
+
+    addExtern<DAS_BIND_FUN(sqlite3_exec_cb)>(*this,lib,"sqlite3_exec",
+        SideEffects::worstDefault, "sqlite3_exec_cb")
+            ->args({"db","sql","errmsg","block","context","at"});
+
+    for ( auto & pfn : this->functions.each() ) {
+        // ok, lets fix up everything returning uint8? into returning string# and make it unsafe operation
+        if ( pfn->result->isPointer() && pfn->result->firstType &&
+                pfn->result->firstType->baseType==Type::tUInt8 && pfn->result->firstType->dim.size()==0 ) {
+            pfn->result = make_smart<TypeDecl>(Type::tString);
+            pfn->result->constant = true;
+            pfn->result->temporary = true;
+            pfn->unsafeOperation = true;
         }
-    });
+        // fixup module functions, so that there is a string cast
+        bool anyString = false;
+        for ( auto & arg : pfn->arguments ) {
+            if ( arg->type->isString() && !arg->type->ref ) {
+                anyString = true;
+            }
+        }
+        if ( anyString ) {
+            pfn->needStringCast = true;
+        }
+    }
 }
 
 ModuleAotType Module_dasSQLITE::aotRequire ( TextWriter & tw ) const {
