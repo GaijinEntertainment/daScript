31f6dca927696e08e6e7c96edb75f8847d5c2362
Boris Batkin <bbatkin@gmail.com>
2024-06-05 04:10:48 -0700

    only fold builtin functions for op1\op2\op3 on default types.

--- src/ast/ast_const_folding.cpp
+++ src/ast/ast_const_folding.cpp
@@ -492,7 +492,7 @@ namespace das {
             if (expr->func->sideEffectFlags) {
                 return Visitor::visit(expr);
             }
-            if ( expr->subexpr->constexpression ) {
+            if ( expr->subexpr->constexpression && expr->func->builtIn ) {
                 if ( expr->type->isFoldable() && expr->subexpr->type->isFoldable() ) {
                     return evalAndFold(expr);
                 }
@@ -504,7 +504,7 @@ namespace das {
             if (expr->func->sideEffectFlags) {
                 return Visitor::visit(expr);
             }
-            if ( expr->left->constexpression && expr->right->constexpression ) {
+            if ( expr->left->constexpression && expr->right->constexpression && expr->func->builtIn ) {
                 if ( expr->type->isFoldable() && expr->left->type->isFoldable() && expr->right->type->isFoldable() ) {
                     return evalAndFold(expr);
                 }
@@ -551,7 +551,7 @@ namespace das {
         }
     // op3
         virtual ExpressionPtr visit ( ExprOp3 * expr ) override {
-            if ( expr->type->isFoldable() && expr->subexpr->constexpression && expr->left->constexpression && expr->right->constexpression ) {
+            if ( expr->type->isFoldable() && expr->subexpr->constexpression && expr->left->constexpression && expr->right->constexpression && expr->func->builtIn ) {
                 return evalAndFold(expr);
             } else if ( expr->type->isFoldable() && expr->subexpr->noSideEffects && expr->left->constexpression && expr->right->constexpression ) {
                 bool failed;
