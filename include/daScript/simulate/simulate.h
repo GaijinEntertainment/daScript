3973c31c91dc66158e75221e40e2615ce86ea556
Boris Batkin <bbatkin@gmail.com>
2024-06-24 12:37:06 -0700

    upsie

--- include/daScript/simulate/simulate.h
+++ include/daScript/simulate/simulate.h
@@ -178,7 +178,7 @@ namespace das
     public:
         dasException ( const char * why, const LineInfo & at )
             : exception(why), exceptionAt(at), exceptionWhat(why) {}
-        virtual char const* what() const {
+        virtual char const* what() const override {
             return exceptionWhat ? exceptionWhat : "unknown exception";
         }
     public:
