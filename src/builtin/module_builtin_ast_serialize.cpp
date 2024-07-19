142e9e713bdf25c41565bc71523ab0eecf56c721
Boris Batkin <bbatkin@gmail.com>
2024-07-18 22:25:58 -0700

    upsie

--- src/builtin/module_builtin_ast_serialize.cpp
+++ src/builtin/module_builtin_ast_serialize.cpp
@@ -2160,7 +2160,7 @@ namespace das {
     }
 
     uint32_t AstSerializer::getVersion () {
-        static constexpr uint32_t currentVersion = 25;
+        static constexpr uint32_t currentVersion = 26;
         return currentVersion;
     }
 
