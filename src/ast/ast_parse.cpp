9330462ba8d8c7ef8c998fc0a23d9520e0f00054
Boris Batkin <bbatkin@gmail.com>
2024-11-05 16:24:33 -0800

    options gen2

--- src/ast/ast_parse.cpp
+++ src/ast/ast_parse.cpp
@@ -410,8 +410,15 @@ namespace das {
                     continue;
                 }
             }
-            if (text[i] == '#' && i + 5 < length && text[i + 1] == 'g' && text[i + 2] == 'e' && text[i + 3] == 'n' && text[i + 4] == '2' && text[i + 5] == '#') {
-                return true;
+            // check for options\s*gen2
+            if (text[i] == 'o' && i + 7 < length && text[i + 1] == 'p' && text[i + 2] == 't' && text[i + 3] == 'i' && text[i + 4] == 'o' && text[i + 5] == 'n' && text[i + 6] == 's' && isspace(text[i + 7]) ) {
+                i += 7;
+                while (i < length && isspace(text[i])) {
+                    ++i;
+                }
+                if (i + 4 < length && text[i] == 'g' && text[i + 1] == 'e' && text[i + 2] == 'n' && text[i + 3] == '2') {
+                    return true;
+                }
             }
         }
         return false;
--- src/parser/lex2.yy.h
+++ src/parser/lex2.yy.h
@@ -718,7 +718,7 @@ extern int yylex \
 #undef yyTABLES_NAME
 #endif
 
-#line 773 "ds2_lexer.lpp"
+#line 772 "ds2_lexer.lpp"
 
 
 #line 724 "lex2.yy.h"
