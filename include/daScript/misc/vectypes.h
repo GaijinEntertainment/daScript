1c6f96ec1038e329ee445ea307d9a9a1c35c781d
Vladimir 'virtul' Ivannikov <v.ivannikov@gaijin.team>
2025-02-05 23:44:48 +0300

    fix aot normalize(float3)
    Apparently use of `v_ldu_p3` (which in the end is expanded in
    simple `_mm_loadu_ps`) causing some compilers (*) to mis-optimize subj
    code (I guessing b/c of load past vector instance?) so use safe version
    which works reliably.
    
    (*) e.g. clang18
    
    Somewhat related to
    https://github.com/GaijinEntertainment/daScript/pull/1457

--- include/daScript/misc/vectypes.h
+++ include/daScript/misc/vectypes.h
@@ -49,8 +49,8 @@ namespace das
     __forceinline vec4f vec_loadu(const float *v) {return v_ldu(v);}
     __forceinline vec4f vec_loadu(const int *v) {return v_cast_vec4f(v_ldui(v));}
     __forceinline vec4f vec_loadu(const unsigned int *v) {return vec_loadu((const int *)v);}
-    __forceinline vec4f vec_loadu3(const float *v) {return v_ldu_p3(v);}
-    __forceinline vec4f vec_loadu3(const int *v) {return v_cast_vec4f(v_ldui_p3(v));}
+    __forceinline vec4f vec_loadu3(const float *v) {return v_ldu_p3_safe(v);}
+    __forceinline vec4f vec_loadu3(const int *v) {return v_cast_vec4f(v_ldui_p3_safe(v));}
     __forceinline vec4f vec_loadu3(const unsigned int *v) {return vec_loadu3((const int *)v);}
     __forceinline vec4f vec_loadu_half(const float *v) {return v_ldu_half(v);}
     __forceinline vec4f vec_loadu_half(const int *v) {return v_cast_vec4f(v_ldui_half(v));}
@@ -75,7 +75,7 @@ namespace das
         __forceinline vec2(vec4f t) : x(vec_extract<TT>::x(t)), y(vec_extract<TT>::y(t)) {}
         __forceinline vec2(TT X, TT Y) : x(X), y(Y) {}
         __forceinline vec2(TT t) : x(t), y(t) {}
-         __forceinline operator vec4f() const { return vec_loadu_half(&x); };
+        __forceinline operator vec4f() const { return vec_loadu_half(&x); };
     };
 
     template <typename TT>
@@ -98,7 +98,6 @@ namespace das
         __forceinline vec3(TT X, TT Y, TT Z) : x(X), y(Y), z(Z) {}
         __forceinline vec3(TT t) : x(t), y(t), z(t) {}
         __forceinline operator vec4f() const { return vec_loadu3(&x); };
-
     };
 
     template <typename TT>
