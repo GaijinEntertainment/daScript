5c239ea89649c9ffd50cfbfa3bc2b188c8d52ec5
Churkin Aleksey <lekhachurkin@gmail.com>
2025-11-27 11:32:34 +0300

    ignore stub in AstAllocate

--- src/ast/ast_allocate_stack.cpp
+++ src/ast/ast_allocate_stack.cpp
@@ -167,6 +167,7 @@ namespace das {
             return true;
         }
         virtual bool canVisitFunction ( Function * fun ) override {
+            if ( fun->stub ) return false;
             if ( fun->isTemplate ) return false;
             if ( fun->stackResolved ) return false;
             if ( !fun->used && !isEverything ) return false;
