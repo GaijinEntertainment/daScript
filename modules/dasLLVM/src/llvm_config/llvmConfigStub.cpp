c907284e76be635e1e9ed43c1efe2f9bf980f0c1
Churkin Aleksey <lekhachurkin@gmail.com>
2025-12-03 12:14:39 +0300

    jit: remove llvm_config module
    We don't need it anymore, since jit config is part of COP.

--- modules/dasLLVM/src/llvm_config/llvmConfigStub.cpp
+++
@@ -1,8 +0,0 @@
-
-bool llvm_jit_enabled () { return false; }
-bool llvm_jit_all_functions () { return false; }
-
-bool llvm_debug_result () { return false; }
-bool llvm_debug_everything () { return false; }
-
-bool llvm_jit_always_solid () { return false; }
