71c9a18fad12d137d640c532c115b008f08351b8
Churkin Aleksey <lekhachurkin@gmail.com>
2026-02-09 10:33:45 +0300

    aot: do not overwrite
    Do not overwrite aot if content is the same. It will make
    rebuilds faster.

--- include/daScript/ast/ast_aot_cpp.h
+++ include/daScript/ast/ast_aot_cpp.h
@@ -4,14 +4,40 @@
 #include "daScript/daScript.h"
 
 namespace das {
+
+    inline bool checkFileContentsIsTheSame ( const string_view & fname, const string_view & str ) {
+        bool file_is_the_same = false;
+        if ( FILE * f = fopen ( fname.data(), "rb" ) )
+        {
+            fseek ( f, 0, SEEK_END );
+            if ( str.length() == ftell ( f ) )
+            {
+                if ( void * buf = malloc ( str.length() ) )
+                {
+                    fseek ( f, 0, SEEK_SET );
+                    if ( fread ( buf, 1, str.length(), f ) == str.length() && memcmp ( str.data(), buf, str.length() ) == 0 )
+                        file_is_the_same = true; // file is the same, don't write!
+                    free ( buf );
+                }
+            }
+            fclose ( f );
+        }
+        return file_is_the_same;
+    }
+
     inline bool saveToFile ( TextPrinter &tout, const string_view & fname, const string_view & str, bool quiet = false ) {
-        if ( !quiet )  {
-            tout << "saving to " << fname.data() << "\n";
+        if ( checkFileContentsIsTheSame ( fname, str ) ) {
+            if ( !quiet )
+                tout << "contents is the same, skip writing to " << fname.data() << "\n";
+            return true;
         }
         FILE * f = fopen ( fname.data(), "w" );
         if ( !f ) {
             tout << "can't open " << fname.data() << "\n";
             return false;
+        }
+        if ( !quiet )  {
+            tout << "saving to " << fname.data() << "\n";
         }
         fwrite ( str.data(), str.length(), 1, f );
         fclose ( f );
