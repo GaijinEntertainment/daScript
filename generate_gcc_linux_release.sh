86d2e79c52eec59dc1d3cb5ae5d67cd289d45615
Churkin Aleksey <lekhachurkin@gmail.com>
2026-03-24 12:57:15 +0300

    compiler: remove unused root files
    There were some files/scripts/configs in the root directory
    that was unused, removed them.

--- generate_gcc_linux_release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -e
-
-rm -r -f cmake_temp
-mkdir cmake_temp
-cd cmake_temp
-
-#-DDAS_CLANG_BIND_DISABLED=FALSE
-cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DDAS_LLVM_DISABLED=FALSE -DDAS_GLFW_DISABLED=TRUE -G Ninja
