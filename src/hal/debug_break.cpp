f97837e9f44904664e663bb63e4491fb08f4b225
Churkin Aleksey <lekhachurkin@gmail.com>
2026-04-29 12:19:14 +0300

    dascript: move globals to libDaScript
    DaScript will not work without these globals. And it makes no
    sense to force user to define them. Let's ship these globals with
    libDaScript.

--- src/hal/debug_break.cpp
+++ src/hal/debug_break.cpp
@@ -5,20 +5,6 @@
 
 #include <signal.h>
 
-uint64_t               das::ptr_ref_count::ref_count_total = 0;
-uint64_t               das::ptr_ref_count::ref_count_track = 0;
-uint64_t               das::ptr_ref_count::ref_count_track_destructor = 0;
-das::ptr_ref_count *   das::ptr_ref_count::ref_count_head = nullptr;
-das::mutex             das::ptr_ref_count::ref_count_mutex;
-
-DAS_API das::atomic<uint64_t> das::g_smart_ptr_total {0};
-
-// JobStatus / Feature tracking
-DAS_API das::atomic<uint64_t> das::g_jobque_track_total {0};
-DAS_API das::atomic<uint64_t> das::g_jobque_track_id {0};
-das::JobStatus *    das::JobStatus::sTrackHead = nullptr;
-das::mutex          das::JobStatus::sTrackMutex;
-
 DAS_API void os_debug_break()
 {
 #ifdef _MSC_VER
