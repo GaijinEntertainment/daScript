17ae9b041bef6df463e1f3972fe396eb1c012d7f
Y0L042 <66077762+Y0L042@users.noreply.github.com>
2026-06-10 13:39:07 +0200

    Fix grammar rule for module name requirements
    Support relative path prefixes (./  and ../) in require declarations.

--- tree-sitter-daslang/grammar.js
+++ tree-sitter-daslang/grammar.js
@@ -176,6 +176,7 @@ module.exports = grammar({
 
     require_module_name: $ => seq(
       optional('%'),
+      optional(seq(choice('..', '.'), '/')),
       $.identifier,
       repeat(seq(choice('.', '/'), $.identifier)),
     ),
